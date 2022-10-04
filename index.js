269d4734d940b45300277343da55829430e2105f
Kajal <kajalbodarya@gmail.com>
2022-10-04 20:04:49 +0530

    create Carbonara recipe

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ console.log('====================================');
 console.log("==============Spaghetti alla Carbonara Recipe=============");
 let spaghetti = 400;
 let guanciale = 250;
+let perEggWeight = 50;
 let fresh_egg_yolks = 6;
 let pecorino_Romano_Cheese = 50;
 let raw_black_pepper = 4;
-
+let totalEggWeight = fresh_egg_yolks * perEggWeight
+let cheeseMixture = totalEggWeight + pecorino_Romano_Cheese;
+let container = spaghetti + guanciale;
+let Carbonara = cheeseMixture + container + raw_black_pepper
 console.log('====================================');
-console.log();
+console.log("Carbonara :", Carbonara);
 console.log('====================================');
