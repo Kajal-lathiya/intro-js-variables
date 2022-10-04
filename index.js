99fca9e33d2d5d9a2947725e6a828bbd7ac0165a
Kajal <kajalbodarya@gmail.com>
2022-10-04 19:47:42 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let fristName = "Kajal";
+let lastName = "lathiya";
+let age = 25;
+let currentYear = 2022
+let BirthYear = currentYear - age
+console.log("--------------******Introduction******----------------");
+console.log('====================================');
+console.log("Hello, My name is", fristName + ' ' + lastName);
+console.log("My birth year is", BirthYear);
+console.log('====================================');
+
+console.log("==============Spaghetti alla Carbonara Recipe=============");
+let spaghetti = 400;
+let guanciale = 250;
+let fresh_egg_yolks = 6;
+let pecorino_Romano_Cheese = 50;
+let raw_black_pepper = 4;
+
+console.log('====================================');
+console.log();
+console.log('====================================');
